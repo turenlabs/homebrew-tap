5e06276a84734770223a0cc05ff35b049311ab78
Tom Bowyer <tbowyer@outlook.com>
2026-05-19 14:09:25 -0500

    Update spice to 0.1.9

--- Formula/spice.rb
+++ Formula/spice.rb
@@ -1,26 +1,26 @@
 class Spice < Formula
   desc "Local Shai-Hulud exposure checker for developers"
   homepage "https://github.com/turenlabs/spice"
-  version "0.1.8"
+  version "0.1.9"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/turenlabs/spice/releases/download/v#{version}/spice_#{version}_darwin_arm64.tar.gz"
-      sha256 "248740c458794a2555b4fcaa01a8ce2ba1f9faef0a15c63f5e6ef5f4a15a657d"
+      sha256 "8c7f6888863dd82278c23e04bd62a58dde55659078601941f8cd2dc6c75a11f6"
     else
       url "https://github.com/turenlabs/spice/releases/download/v#{version}/spice_#{version}_darwin_amd64.tar.gz"
-      sha256 "92a02918a06a3fa3d846eea63e3b942f63ecbf6c99373234fa45354a7c2840af"
+      sha256 "b4a7da6e245d24c41ec11a6cf27a04e18a3f7a405e6f2a01bf6b1e56593632ff"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/turenlabs/spice/releases/download/v#{version}/spice_#{version}_linux_arm64.tar.gz"
-      sha256 "39d9f955598e8577d01bbee93d3f9606c203d89a45a61c5b52ca9d359527a6d5"
+      sha256 "f56bea71150f3e15d9c4081df6f55637b6893edf6bb43b8d37d567ba3ddd22bd"
     else
       url "https://github.com/turenlabs/spice/releases/download/v#{version}/spice_#{version}_linux_amd64.tar.gz"
-      sha256 "e1e428ca77e6cdcb26bef8aea961bddfeb5c184abb16a2db4be65c93abafd189"
+      sha256 "8748139bcc69910c549b70f432ca902716166c6f2c6ae28cbe81329a217afcd2"
     end
   end
 
